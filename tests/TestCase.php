bb1986ea8e988faecad4987cbf2687ac99d6cb68
dinh.tai <dinh.tai>
2017-11-28 16:11:37 +0700

    Integration Test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use App\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setAuthUser(array $attributes = [])
+    {
+        $user = factory(User::class)->create($attributes);
+        $this->actingAs($user, 'web');
+
+        return $user;
+    }
 }
